517a82f1afd411a5a94e3a19fe616043891297cb
MasaburovaSati <yasinavakhapova@gmail.com>
2023-03-29 15:40:08 -0500

    Login in studyMate.feature

---
+++ src/test/java/steps/StudyMateLogin_steps.java
@@ -0,0 +1,35 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import pages.StudyMateLoginPage;
+import utilities.Config;
+import utilities.Driver;
+import utilities.Flow;
+
+public class StudyMateLogin_steps {
+
+    StudyMateLoginPage studyMateLoginPage = new StudyMateLoginPage();
+
+
+    @Given("user on study mate application")
+    public void user_on_study_mate_application() {
+       Driver.getDriver().get(Config.getValue("studyMateURL"));
+
+
+
+    }
+    @Given("user enters credentials {string} and {string} and clicks login")
+    public void user_enters_credentials_and_and_clicks_login(String email, String password) {
+        studyMateLoginPage.emailInputBox.sendKeys("studyMateLoginEmail");
+        studyMateLoginPage.passwordInputBox.sendKeys("studyMateLoginPassword");
+        studyMateLoginPage.loginButtonS.click();
+        Flow.wait(500);
+
+    }
+    @Then("user should be successfully login to application")
+    public void user_should_be_successfully_login_to_application() {
+
+
+    }
+}
---
+++ src/test/resources/features/studyMate.feature
@@ -0,0 +1,9 @@
+Feature: Study Mate Login
+
+
+  @login
+  Scenario:  Verifying the login functionality
+    Given user on study mate application
+    And user enters credentials "email" and "password" and clicks login
+    Then user should be successfully login to application
+
